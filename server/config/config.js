2b4fb1f4a6e0fac0b8273357c86103abfe03fb5f
yali3391 <castroarely92@gmail.com>
2019-11-19 20:44:45 -0600

    variable entorno

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV=='dev'){
     urlDB='mongodb://localhost:27017/cafe'
 }
 else{
-    urlDB=process.env.MONGO_URI;
+    urlDB=process.env.MONGOURI;
 }
 
 process.env.urlDB=urlDB;
